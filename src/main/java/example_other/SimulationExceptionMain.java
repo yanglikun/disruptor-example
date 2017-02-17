a2c383cd4d90b1dd3a991d3e2ee7586cad3c2895
yanglikun <yanglikun@jd.com>
2017-02-17 11:37:36 +0800

    添加异常处理example

--- src/main/java/example_other/SimulationExceptionMain.java
+++ src/main/java/example_other/SimulationExceptionMain.java
@@ -13,25 +13,26 @@ public class SimulationExceptionMain extends MainTemplate {
 
     public void addHandler(Disruptor<GenericEvent<String>> disruptor) {
         disruptor.handleEventsWith(new GenericExceptionEventHandler<String>("handler-1"));
-        disruptor.setDefaultExceptionHandler(new ExceptionHandler() {
-
-            public void handleEventException(Throwable ex, long sequence, Object event) {
-                System.err.println("处理发生异常:"+event);
-            }
-
-            public void handleOnStartException(Throwable ex) {
-                System.err.println("启动发生异常:"+ex);
-
-
-            }
-
-            public void handleOnShutdownException(Throwable ex) {
-                System.err.println("停止发生异常:"+ex);
-            }
-        });
+        disruptor.setDefaultExceptionHandler(new SimpleExceptionHandler());
     }
 
     public static void main(String[] args) {
         new SimulationExceptionMain().run();
+    }
+
+    private static class SimpleExceptionHandler implements ExceptionHandler {
+        public void handleEventException(Throwable ex, long sequence, Object event) {
+            System.err.println("处理发生异常:" + event);
+        }
+
+        public void handleOnStartException(Throwable ex) {
+            System.err.println("启动发生异常:" + ex.getMessage());
+            ex.printStackTrace();
+        }
+
+        public void handleOnShutdownException(Throwable ex) {
+            System.err.println("停止发生异常:" + ex.getMessage());
+            ex.printStackTrace();
+        }
     }
 }
